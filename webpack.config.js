a921e67023992b1fd8294dff0f04dceeae641be4
BUDAPEST\Rami_Amayri <ramiamaire@abv.bg>
2017-09-20 16:33:19 +0300

    Adding boilerplate to master

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: { 
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        //contentBase: path.resolve(__dirname),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-1']
+                }
+            }
+        ]
+    }
+}
